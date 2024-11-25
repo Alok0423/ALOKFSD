fa66f8caf8238e0b84eea00d7cc32e6be3c91bfa
alok0423 <ishuu379@gmail.com>
2024-11-25 21:37:17 +0530

    changes

---
+++ aimlbreactvite/src/ImageMnupulation.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ImageMnupulation() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ImageMnupulation
--- aimlbreactvite/src/StudentState.jsx
+++ aimlbreactvite/src/StudentState.jsx
@@ -1,4 +1,27 @@
-import  React,{useState} from 'react'
-function StudentState(){
-    const[count,set[Count]=useState(20);]
+import React,{useState} from 'react'
+
+function StudentState() {
+
+    const [count,setcount]=useState(15);
+
+    const increment = ()=>{
+        setcount((prev)=>prev+1);
+    }
+
+    const decrement = ()=>{
+        setcount((prev)=>prev-1);
+    }
+
+    
+
+
+  return (
+    <>
+    <div>Counter : {count}</div>
+    <button onClick={increment}>Increase</button>
+    <button onClick={decrement}>Decrement</button>
+    </>
+  )
 }
+
+export default StudentState
